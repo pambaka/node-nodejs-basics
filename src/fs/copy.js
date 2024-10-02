218231a07ccdf04ea5e92dd2ab2159eb4f062f60
pambaka <pambaka@ya.ru>
2024-10-02 23:26:16 +0200

    feat: fs / implement 'copy' function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import getDirname from "./getDirname.js";
+import { errorMessage, folderName } from "./const.js";
+
 const copy = async () => {
-    // Write your code here 
+  const newFolderName = "files_copy";
+
+  const __dirname = getDirname();
+  const srcFolder = path.join(__dirname, folderName);
+  const destFolder = path.join(__dirname, newFolderName);
+
+  await fs
+    .cp(srcFolder, destFolder, {
+      force: false,
+      errorOnExist: true,
+      recursive: true,
+    })
+    .catch(() => {
+      throw new Error(errorMessage);
+    });
 };
 
 await copy();
