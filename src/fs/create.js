30bb904d381d7220fb6c349b1faf303d59ce72f3
pambaka <pambaka@ya.ru>
2024-10-02 19:40:05 +0200

    feat: fs / implement 'create' function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const fileName = "fresh.txt";
+  const fileContent = "I am fresh and young";
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.join(__dirname, "files", fileName);
+
+  fs.writeFile(filePath, fileContent, { flag: "wx+" }).catch(() => {
+    throw new Error("FS operation failed");
+  });
 };
 
 await create();
