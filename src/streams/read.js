759344697592af4d4fd5d43e4a172ae0d096770a
pambaka <pambaka@ya.ru>
2024-10-04 09:20:47 +0200

    feat: streams / implement 'read' function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import os from "os";
+
 const read = async () => {
-    // Write your code here 
+  const fileName = "fileToRead.txt";
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files", fileName);
+
+  const readStream = fs.createReadStream(filePath, { encoding: "utf8" });
+  readStream.on("data", (chunk) => process.stdout.write(`${chunk}${os.EOL}`));
 };
 
 await read();
