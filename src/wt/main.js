3d2d5c149cd34e72cabaf81b2de5bac21d2e81e7
pambaka <pambaka@ya.ru>
2024-10-06 23:46:30 +0200

    feat: wt / worker threads functions are implemented

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import os from "node:os";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { Worker } from "node:worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const workerScript = path.join(__dirname, "worker.js");
+
+  const createWorker = (workerData) =>
+    new Promise((resolve) => {
+      const worker = new Worker(workerScript, { workerData });
+      worker.on("message", (value) =>
+        resolve({ status: "resolved", data: value })
+      );
+      worker.on("error", () => resolve({ stauts: "error", data: null }));
+    });
+
+  const workers = [];
+  const cpusNumber = os.cpus().length;
+  for (let i = 0; i < cpusNumber; i += 1) {
+    workers.push(createWorker(10 + i));
+  }
+
+  await Promise.all(workers).then((results) => console.log(results));
 };
 
 await performCalculations();
