8e34ec21a9a916131f162aaadda513b3daec023b
pambaka <pambaka@ya.ru>
2024-10-07 12:31:10 +0200

    feat: cp / implement child process function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import child_process from "node:child_process";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const childScriptPath = path.join(__dirname, "files", "script.js");
+
+  const child = child_process.spawn("node", [childScriptPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.on("data", (data) => process.stdout.write(`${data}`));
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, "a", 2, "b"]);
